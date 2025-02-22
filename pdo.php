f854f9ac488c195fa8c811bc8311086ce92b6ac5
habib motavassel <habib.motavassel@gmail.com>
2025-02-22 18:27:37 +0100

    To modify README file

--- pdo.php
+++ pdo.php
@@ -1,13 +1,4 @@
 <?php
-$host = "postgresql://fred:vlNkSyzAyIkQl0MMeVDdNMwfwLckGSoo@dpg-cusv4gbqf0us739ponsg-a/misc_yo7z";
-$dbname = "misc_yo7z";
-$user = "fred";
-$pass = "vlNkSyzAyIkQl0MMeVDdNMwfwLckGSoo";
-
-try {
-    $pdo = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=if0_38374076_misc','fred', 'zap');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
